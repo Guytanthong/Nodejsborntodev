bfeea5518ff80fe38ae7613cddcf7cec06aa4037
Guytanthong <tanthongguy292@gmail.com>
2026-03-02 10:47:43 +0700

    new template

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const chalk = require('chalk');
 const app = express();
+const morgan = require('morgan');
 const port = 3000;
+const path = require('path');
 
+
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/")));
 app.get("/", (req,res) =>{
 
     res.send('Hello Guy');

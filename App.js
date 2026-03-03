b9d3f62bff85b5312024b19e9ceea318adbbab61
Guytanthong <tanthongguy292@gmail.com>
2026-03-03 08:41:58 +0700

    change to ejs nodemon

--- App.js
+++ App.js
@@ -2,18 +2,21 @@ const express = require('express');
 const chalk = require('chalk');
 const app = express();
 const morgan = require('morgan');
-const port = 3000;
+const port = process.env.PORT;
 const path = require('path');
 
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")));
+app.set("views", "./src/views");
+app.set("view engine", "ejs")
+
 app.get("/", (req,res) =>{
 
-    res.send('Hello Guy');
+    res.render('index', {username: 'Guy', customer: ["git","ddd","jae"]});
 
 })
 
 app.listen(port, () => {
-    console.log("Listening on port port" + chalk.green(' ' + port));
+    console.log("Listening on port" + chalk.green(' ' + port));
 })

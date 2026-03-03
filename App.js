5bfd4e4affff3d997c1a3e4286715e36f7813d10
Guytanthong <tanthongguy292@gmail.com>
2026-03-03 08:47:04 +0700

    Update App.js

--- App.js
+++ App.js
@@ -4,13 +4,24 @@ const app = express();
 const morgan = require('morgan');
 const port = process.env.PORT;
 const path = require('path');
-
+const productRouter = express.Router();
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")));
 app.set("views", "./src/views");
 app.set("view engine", "ejs")
 
+
+productRouter.route("/").get((req, res) => {
+    res.send("Hello world Im Products");
+});
+
+productRouter.route("/1").get((req, res) => {
+    res.send("Hello world Im Products1");
+});
+
+
+app.use("/products", productRouter)
 app.get("/", (req,res) =>{
 
     res.render('index', {username: 'Guy', customer: ["git","ddd","jae"]});
